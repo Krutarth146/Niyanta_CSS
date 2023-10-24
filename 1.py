5fb389b288387026f7b7051357fc7c052e448cf9
Krutarth Patel <krutarthpatel146@gmail.com>
2023-10-24 13:04:48 +0530

    Position 24_10

--- 1.py
+++ 1.py
@@ -1,20 +1,31 @@
-list1 =  [(1,2), (2,1), (1,2), (1,1)]
+# list1 =  [(1,2), (2,1), (1,2), (1,1)]
 
-def domninocycle(tiles):
-    if len(tiles)==0:
-        return True
+# def domninocycle(tiles):
+#     if len(tiles)==0:
+#         return True
 
-    flag = 0
-    for i in range(len(tiles) - 1):
-        if tiles[i][-1] != tiles[i + 1][0]:
-            flag = 1
+#     flag = 0
+#     for i in range(len(tiles) - 1):
+#         if tiles[i][-1] != tiles[i + 1][0]:
+#             flag = 1
 
-    if flag == 0:
-        if tiles[0][0] == tiles[-1][1]:
-            return True
-        else:
-            return False
-    else:
-        return False
+#     if flag == 0:
+#         if tiles[0][0] == tiles[-1][1]:
+#             return True
+#         else:
+#             return False
+#     else:
+#         return False
     
-print(domninocycle(list1))
+# print(domninocycle(list1))
+
+
+
+# Program to depict Raising Exception 
+  
+try:  
+    print("Hello")
+    raise NameError("Name Erroorroror")  # Raise Error 
+except NameError: 
+    print ("An exception") 
+    raise  # To determine whether the exception was raised or not 
